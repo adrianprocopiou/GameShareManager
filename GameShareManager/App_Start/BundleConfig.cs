9d9d3efad1a94c8eda765220137ee96892bd8273
Adrian Procopiou <adrianprocopiou@gmail.com>
2018-06-16 23:40:50 -0400

    Added web app theme

--- GameShareManager/App_Start/BundleConfig.cs
+++ GameShareManager/App_Start/BundleConfig.cs
@@ -22,9 +22,29 @@ namespace GameShareManager
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            // Inspinia script
+            bundles.Add(new ScriptBundle("~/bundles/inspinia").Include(
+                "~/Scripts/app/inspinia.js"));
+
+            // SlimScroll
+            bundles.Add(new ScriptBundle("~/plugins/slimScroll").Include(
+                "~/Scripts/plugins/slimScroll/jquery.slimscroll.min.js"));
+
+            // jQuery plugins
+            bundles.Add(new ScriptBundle("~/plugins/metsiMenu").Include(
+                "~/Scripts/plugins/metisMenu/metisMenu.min.js"));
+
+            bundles.Add(new ScriptBundle("~/plugins/pace").Include(
+                "~/Scripts/plugins/pace/pace.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/animate.css",
+                      "~/Content/style.css"));
+
+            // Font Awesome icons
+            bundles.Add(new StyleBundle("~/font-awesome/css").Include(
+                "~/fonts/font-awesome/css/fontawesome-all.min.css", new CssRewriteUrlTransform()));
         }
     }
 }
